0a94dca29c0b7250671b3d1ffe768fc31709e21e
cakirhalil <halilcakir343@gmail.com>
2024-02-13 17:10:27 +0300

    destructor method created

--- python.py
+++ python.py
@@ -1,1 +1,7 @@
 class Library:
+    def __init__(self):
+        self.file_path = "books.txt"
+        self.file = open(self.file_path, "a+")
+        
+    def __del__(self):
+        self.file.close()      
