2d3a59219e4da7c8b438851f40a6236c1e64c73e
cakirhalil <halilcakir343@gmail.com>
2024-02-16 20:21:50 +0300

    comment lines

--- python.py
+++ python.py
@@ -6,36 +6,41 @@ class Library:
     def __del__(self):
         self.file.close()      
     
+
+    # kitapları listeleme
     def list_books(self):
         self.file.seek(0)
         lines = self.file.read().splitlines()
         for line in lines:
             record = line.split(',')
-            book_name, author, category, release_date, num_pages = record
+            book_name, author, category, release_date, number_of_pages = record
             print(f"Kitap Adı: {book_name}, Yazar: {author}")
 
+    # kitap ekleme
     def add_book(self):
         book_name = input("Kitab adını girin:  ")
         author = input("Yazar adını girin:  ")
         category = input("Kitap kategorisini girin: ")
         release_date = input("Yayınlanma tarihini girin:  ")
-        num_pages = input("Kitap sayfa sayısını girin:  ")
+        number_of_pages = input("Kitap sayfa sayısını girin:  ")
 
-        record = f"{book_name},{author},{category},{release_date},{num_pages}\n"
+        record = f"{book_name},{author},{category},{release_date},{number_of_pages}\n"
         self.file.write(record)
         print(f"'{book_name}' kitabı eklendiii")
 
+    # kitap silme
+
     def remove_book(self):
-        title_to_remove = input("Silinecek olan kitabın adını girin: ")
+        remove_book_name = input("Silinecek olan kitabın adını girin: ")
         lines = self.file.readlines()
         self.file.seek(0)
         self.file.truncate()
 
         for line in lines:
-            if title_to_remove not in line:
+            if remove_book_name not in line:
                 self.file.write(line)
 
-        print(f"'{title_to_remove}' kitabı listeden silindiii")
+        print(f"'{remove_book_name}' kitabı listeden silindiii")
 
 
 lib = Library()
